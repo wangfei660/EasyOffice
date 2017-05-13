37099240557a73246737f420f2538e312704afd8
Freed <buscando_alink@hotmail.com>
2017-05-13 01:09:12 -0400

    Add Initial Methods

--- src/ppmaker/PPMaker.java
+++ src/ppmaker/PPMaker.java
@@ -9,17 +9,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import org.apache.poi.xslf.usermodel.SlideLayout;
+import java.util.HashMap;
 
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
-import org.apache.poi.xslf.usermodel.XSLFNotes;
-import org.apache.poi.xslf.usermodel.XSLFShape;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 import org.apache.poi.xslf.usermodel.XSLFSlideLayout;
 import org.apache.poi.xslf.usermodel.XSLFSlideMaster;
-import org.apache.poi.xslf.usermodel.XSLFTextBox;
-import org.apache.poi.xslf.usermodel.XSLFTextParagraph;
-import org.apache.poi.xslf.usermodel.XSLFTextShape;
 
 /**
  *
@@ -27,72 +22,56 @@ import org.apache.poi.xslf.usermodel.XSLFTextShape;
  */
 public class PPMaker {
 
-    static void edit() throws IOException {
+    static String EXAMPLE_PPT_NAME = "example1.pptx";
 
-        //opening an existing slide show
-        File file = new File("example1.pptx");
-        FileInputStream inputstream = new FileInputStream(file);
-        XMLSlideShow ppt = new XMLSlideShow(inputstream);
+    private static XMLSlideShow openPpt(File pptFile) {
+        XMLSlideShow ppt = null;
 
-        //adding slides to the slodeshow
-        XSLFSlide slide1 = ppt.createSlide();
-        XSLFSlide slide2 = ppt.createSlide();
-
-        //saving the changes 
-        FileOutputStream out = new FileOutputStream(file);
-        ppt.write(out);
-
-        System.out.println("Presentation edited successfully");
-        out.close();
-    }
-
-    public static void create() throws IOException {
-        //creating a new empty slide show
-        XMLSlideShow ppt = new XMLSlideShow();
-
-        //creating an FileOutputStream object
-        File file = new File("example1.pptx");
-        FileOutputStream out = new FileOutputStream(file);
-
-        //saving the changes to a file
-        ppt.write(out);
-        System.out.println("Presentation created successfully");
-        out.close();
-    }
-
-    public static void prueba() throws IOException {
-        File file = new File("example1.pptx");
-        FileInputStream inputstream = new FileInputStream(file);
-        XMLSlideShow ppt = new XMLSlideShow(inputstream);
-
-        for (XSLFSlide slide : ppt.getSlides()) {
-            for (XSLFShape shape : slide.getShapes()) {
-                if (shape instanceof XSLFTextShape) {
-                    XSLFTextShape textShape = (XSLFTextShape) shape;
-                    System.out.println(textShape.getText());
-                }
-            }
-            try {
-
-                for (XSLFShape shape : slide.getNotes()) {
-                    if (shape instanceof XSLFTextShape) {
-                        XSLFTextShape txShape = (XSLFTextShape) shape;
-                        for (XSLFTextParagraph xslfParagraph : txShape.getTextParagraphs()) {
-                            System.out.println(xslfParagraph.getText());
-                        }
-                    }
-                }
-            } catch (Exception e) {
-            }
-
+        try {
+            FileInputStream inputstream = new FileInputStream(pptFile);
+            ppt = new XMLSlideShow(inputstream);
+        } catch (IOException ex) {
+            System.out.println("La presentacion seleccionada no existe");
+            System.exit(-1);
         }
 
-//        FileOutputStream out = new FileOutputStream(file);
-//        ppt.write(out);
-//        out.close();
+        return ppt;
+    }
+
+    private static void savePpt(File file, XMLSlideShow ppt) {
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            ppt.write(out);
+            out.close();
+            System.out.println("La Presentacion guardada");
+        } catch (IOException ex) {
+            System.err.println("La Presentacion no puede ser guardada");
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    private static String replaceContent(String content, HashMap<String, String> data) {
+        return content.replace(String.format("@%s@", data.get("key")), data.get("value"));
+    }
+
+    public static XSLFSlide copySlide(XMLSlideShow ppt, XSLFSlide srcSlide){
+        XSLFSlideLayout layout = srcSlide.getSlideLayout();
+        XSLFSlide newSlide = ppt.createSlide(layout);
+
+        return newSlide.importContent(srcSlide);
     }
 
     public static void main(String[] args) throws IOException {
-        prueba();
+        File f = new File(EXAMPLE_PPT_NAME);
+        XMLSlideShow ppt = openPpt(f);
+
+        XSLFSlide srcSlide = ppt.getSlides().get(0);
+        
+        copySlide(ppt, srcSlide);
+        copySlide(ppt, srcSlide);
+        copySlide(ppt, srcSlide);
+        copySlide(ppt, srcSlide);
+
+        savePpt(f, ppt);
     }
 }
